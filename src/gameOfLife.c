1151657f0cfd5989df2683578de3f3ad9e2e754c
mosquito <danielkmb2@gmail.com>
2011-11-13 21:40:07 +0100

    la hemos liado...

--- src/gameOfLife.c
+++ src/gameOfLife.c
@@ -13,15 +13,6 @@ struct cell
 
 void generateBoard(struct cell v[X][Y])
 {
-	//inicializar el struct cell con neighbours = 0!
-	int i, j;
-	for (i=0;i<X;i++)
-	{
-		for (j=0;j<Y;j++)
-		{
-			v[i][j].neighbours = 0;
-		}
-	}
 	/*
 	0|\/|G H4x0R!!!!!!1111
 	  
@@ -34,11 +25,13 @@ void generateBoard(struct cell v[X][Y])
 	 i+1| # | # | # |
 	    +-----------
 	*/
+	int i,j;
 	for (i=0;i<X;i++)
 	{
 		for (j=0;j<Y;j++)
 		{
 			v[i][j].alive = 0;
+			v[i][j].neighbours = 0;
 		}
 	}
 	v[14][15].alive = 1;
@@ -48,13 +41,14 @@ void generateBoard(struct cell v[X][Y])
 	v[16][16].alive = 1;
 }
 
-void updateBoard (struct cell v[X][Y])
+void updateBoard (struct cell v[X][Y], struct cell v1[X][Y])
 {
 	int i, j;
 	char neighbourCounter = 0;
+	struct cell auxCell;
 	for (i=0;i<X;i++)
 	{
-		for (j=0;j<Y;j++)		
+		for (j=0;j<Y;j++)
 		{
 			//some better ways but...
 			neighbourCounter = 0;
@@ -66,33 +60,39 @@ void updateBoard (struct cell v[X][Y])
 			if (v[(i+1)%X][(j-1)%Y].alive == 1)	neighbourCounter += 1;
 			if (v[(i+1)%X][j%Y].alive == 1)		neighbourCounter += 1;
 			if (v[(i+1)%X][(j+1)%Y].alive == 1)	neighbourCounter += 1;
-			v[i][j].neighbours = neighbourCounter;
-		}
-	}
-	for (i=0;i<X;i++)
-	{
-		for (j=0;j<Y;j++)
-		{
-			if (v[i%X][j%Y].alive == 1)
+			v[i%X][j%Y].neighbours = neighbourCounter;
+		
+
+			auxCell = v[i%X][j%Y];
+			if (auxCell.alive == 1)
 			{
-				if ((v[i%X][j%Y].neighbours == 2)||(v[i%X][j%Y].neighbours == 3))
+				if ((auxCell.neighbours == 2)||(auxCell.neighbours == 3))
 				{
-					v[i%X][j%Y].alive = 1;	//yeeeeeeeeeeeeeaahhhhh
-				} else if ((v[i%X][j%Y].neighbours < 2)||(v[i%X][j%Y].neighbours > 3))
+					v1[i%X][j%Y].alive = 1;		//yeeeeeeeeeeeeeaahhhhh
+				} else if ((auxCell.neighbours < 2)||(auxCell.neighbours > 3))
 				{
-					v[i%X][j%Y].alive = 0;
-					mvaddstr(i, j, " ");
+					v1[i%X][j%Y].alive = 0;
+					mvaddstr(i%X, j%Y, " ");	//output
 				}
-			} else if ((v[i%X][j%Y].alive == 0)&&(v[i%X][j%Y].neighbours == 3))
+			} else if ((auxCell.alive == 0)&&(auxCell.neighbours == 3))
 			{
-				v[i%X][j%Y].alive = 1;
-				mvaddstr(i, j, "O");
+				v1[i%X][j%Y].alive = 1;
+				mvaddstr(i%X, j%Y, "O");		//output
+			} else
+			{
+				v1[i%X][j%Y].alive = v[i%X][j%Y].alive;	
 			}
-			//DUNNO reinicializar neighbours de paso...
-			v[i][j].neighbours=0;
 			refresh();
 		}
 	}
+	/*
+		operamos con v cada recorrido de la matriz, la nueva condición de viva o muerta se guardará en v1, 
+		al acabar cada recorrido v y v1 se intercambian de la forma más ineficiente posible....
+
+		en v1 tendremos la posicion actualizada de todas las celulas, si están vivas o muertas, 
+		y sobre v se hacen todas las operaciones para no liarla
+	*/
+	v=v1;
 }
 
 void initializeScreen()
@@ -108,12 +108,14 @@ void initializeScreen()
 int main()
 {
 	struct cell board[X][Y];
+	struct cell auxBoard[X][Y];
 	initializeScreen();		//useless but cool
 	generateBoard(board);		//with multiple functions using arguments coming soon
 	while (1)
 	{
-		updateBoard(board);
-		usleep(1000);		//or pressing a button 4 debugging
+		usleep(100000);
+		updateBoard(board,auxBoard);
+					//or pressing a button 4 debugging
 	}				//not implemented yed (V)(;,,;)(V)
 	return 0;
 }

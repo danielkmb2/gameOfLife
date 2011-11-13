aef8dca6caaedbdc46aebc3edee6294c8933a43d
mosquito <danielkmb2@gmail.com>
2011-11-13 03:32:18 +0100

    preAlpha con broza

--- src/gameOfLife.c
+++ src/gameOfLife.c
@@ -1,3 +1,11 @@
+/*
+	TO-DO LIST
+	
+	DETECTED PROBLEMS
+		consumo cpu
+		wtf borders
+		no quiero ver el puto cursor saltando
+*/
 #include <stdio.h>
 #include <stdlib.h>
 #include <unistd.h>
@@ -22,10 +30,10 @@ void generateBoard(struct cell v[X][Y])
 			v[i][j].neighbours = 0;
 		}
 	}
-	//generar el tablero aleatoriamente, o de algún otro modo alucinante	
-	
-	//hurdur testing flying hacker logo
-	/*   j-1  j  j+1
+	/*
+	0|\/|G H4x0R!!!!!!1111
+	  
+	     j-1  j  j+1
 	    +---+---+---
 	 i-1|   | # |   |
 	    +-----------	
@@ -50,22 +58,14 @@ void generateBoard(struct cell v[X][Y])
 
 void updateBoard (struct cell v[X][Y])
 {
-	/*
-		buscar todas las posiciones del vector y mirar que hay en sus vecinos
-		para cada posición de board, mirar cuántos vecinos hay en la matrix 3x3 que la rodea
-		if cell=0 && vecinos == 3 then cell = 1
-		if cell=1 && vecinos == 2 || vecinos == 3 then cell = 1
-		if cell=1 && vecinos > 3 || vecinos < 2 then cell = 0
-		OPERAR %X%Y!!!!!!!!!!!
-	*/
 	int i, j;
 	char neighbourCounter = 0;
 	for (i=0;i<X;i++)
 	{
-		for (j=0;j<Y;j++)
+		for (j=0;j<Y;j++)		
 		{
+			//some better ways but...
 			neighbourCounter = 0;
-			//0|\/|G H4x0R!!!!!!1111
 			if (v[(i-1)%X][(j-1)%Y].alive == 1)	neighbourCounter += 1;
 			if (v[(i-1)%X][j%Y].alive == 1) 	neighbourCounter += 1;
 			if (v[(i-1)%X][(j+1)%Y].alive == 1)	neighbourCounter += 1;
@@ -77,8 +77,6 @@ void updateBoard (struct cell v[X][Y])
 			v[i][j].neighbours = neighbourCounter;
 		}
 	}
-	//y recorremos de nuevo toda la matriz FTW... actualizamos el estado esta vez
-	//aqui no uso el módulo verdad?
 	for (i=0;i<X;i++)
 	{
 		for (j=0;j<Y;j++)
@@ -87,42 +85,24 @@ void updateBoard (struct cell v[X][Y])
 			{
 				if ((v[i%X][j%Y].neighbours == 2)||(v[i%X][j%Y].neighbours == 3))
 				{
-					v[i%X][j%Y].alive = 1;
+					v[i%X][j%Y].alive = 1;	//yeeeeeeeeeeeeeaahhhhh
 				} else if ((v[i%X][j%Y].neighbours < 2)||(v[i%X][j%Y].neighbours > 3))
 				{
 					v[i%X][j%Y].alive = 0;
+					mvaddstr(i, j, " ");
 				}
 			} else if ((v[i%X][j%Y].alive == 0)&&(v[i%X][j%Y].neighbours == 3))
 			{
 				v[i%X][j%Y].alive = 1;
-			}
-
-			//DUNNO reinicializar neighbours
-			v[i][j].neighbours=0;
-		}
-	}
-}
-
-void printBoard (struct cell v[X][Y])
-{
-	//brozear la pantalla
-	int i,j;
-	for (i=0;i<X;i++)
-	{
-		for (j=0;j<Y;j++)
-		{
-			if (v[i][j].alive == 1)
-			{
 				mvaddstr(i, j, "O");
-			} else
-			{
-				mvaddstr(i, j, ".");
 			}
+			//DUNNO reinicializar neighbours de paso...
+			v[i][j].neighbours=0;
 			refresh();
 		}
 	}
-
 }
+
 void initializeScreen()
 {
 	WINDOW * mainwin;
@@ -132,27 +112,16 @@ void initializeScreen()
 		exit(EXIT_FAILURE);
 	}
 }
-/*
-void endPrinting()
-{
-	WINDOW * mainwin;
-	delwin(mainwin);
-	endwin();
-	refresh();
-}
-*/
+
 int main()
 {
 	struct cell board[X][Y];
-	initializeScreen();
-	generateBoard(board);
+	initializeScreen();		//useless but cool
+	generateBoard(board);		//with multiple functions using arguments coming soon
 	while (1)
 	{
-		printBoard(board);
 		updateBoard(board);
 		usleep(1000);		//or pressing a button 4 debugging
-					//not implemented yed (V)(;,,;)(V)
-	}
-//	endPrinting();			//tiene gracia porque esto no se va a ejecutar nunca
+	}				//not implemented yed (V)(;,,;)(V)
 	return 0;
 }
